29d04ee378faae3e3026a0bcb4c20a48614d4880
leorustice <116230360+leorustice@users.noreply.github.com>
2022-11-09 22:32:10 +0000

    inicio aula 3

--- Aula3-2022-11-09.py
+++ Aula3-2022-11-09.py
@@ -1,1 +1,10 @@
 print("Início da aula 3 (09/11/2023")
+
+contador = 1
+
+while(contador <= 10):
+  print(contador)
+  contador = contador+1 #contador += 1
+print(contador)
+contador +=1
+print(contador)
